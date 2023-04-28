43ae4f5d3f587eaff8f4230e14f9bd97faaca7ec
porag-m06 <porag.m06@gmail.com>
2023-04-28 16:58:58 +0600

    js added for dynamic featured teacher array of objects

---
+++ scripts/data.js
@@ -0,0 +1,46 @@
+let mentors = [
+    {
+        detailsLink:'#',
+        imgSrc:'assets/mentors/m1.png',
+        name:'',
+        designation:'',
+        summary:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, impedit.'
+    },
+    {
+        detailsLink:'#',
+        imgSrc:'assets/mentors/m2.png',
+        name:'',
+        designation:'',
+        summary:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, impedit.'
+    },
+    {
+        detailsLink:'#',
+        imgSrc:'assets/mentors/m3.png',
+        name:'',
+        designation:'',
+        summary:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, impedit.'
+    },
+    {
+        detailsLink:'#',
+        imgSrc:'assets/mentors/m4.png',
+        name:'',
+        designation:'',
+        summary:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, impedit.'
+    },
+    {
+        detailsLink:'#',
+        imgSrc:'assets/mentors/m5.png',
+        name:'',
+        designation:'',
+        summary:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, impedit.'
+    },
+    {
+        detailsLink:'#',
+        imgSrc:'assets/mentors/m6.png',
+        name:'',
+        designation:'',
+        summary:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, impedit.'
+    }
+]
+
+export default mentors;
