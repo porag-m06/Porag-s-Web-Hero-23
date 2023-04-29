c6a9b54baa685bdf67bcbf6aef83c67053fa296d
porag-m06 <porag.m06@gmail.com>
2023-04-29 14:16:42 +0600

    final modifications 1

--- scripts/script-about.js
+++ scripts/script-about.js
@@ -1,22 +1,3 @@
-import mentors from './data.js';
-
-const mtrGrid = document.querySelector('.mentor');
-
-for (let i = 0; i < mentors.length; i += 1) {
-  const mtrDiv = document.createElement('div');
-  mtrDiv.innerHTML = `<a href="${mentors[i].detailsLink}">
-            <img src=${mentors[i].imgSrc} alt="">
-            <div class="mtr-detail">
-                <h3>${mentors[i].name}</h3>
-                <h5> ${mentors[i].designation}</h5>
-                <div class="line"></div>
-                <p>${mentors[i].summary}</p>
-            </div>
-        </a>`;
-
-  mtrGrid.append(mtrDiv);
-}
-
 // -------- mobile menu navigation STARTS --------
 const navMenu = document.querySelector('.nav2');
 const menuIcon = document.querySelector('.menu');
